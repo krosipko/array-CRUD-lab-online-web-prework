ba2ea7a8756d5fff68701f12d4d8952f1e89d0ec
Kyle Rosipko <krosipko@outlook.com>
2019-01-15 05:59:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,16 +8,14 @@ end
 
 def add_element_to_end_of_array(array, element)
   array << element
-  
 end
 
 def add_element_to_start_of_array(array, element)
   array.unshift(element)
-  
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop  
 end
 
 def remove_element_from_start_of_array(array)
